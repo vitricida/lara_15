4e894029e32a1d2f5d32c2f2e35e14236000aa72
Pierre Poumayrac <pierre.poumayrac@agrotoken.io>
2025-08-13 00:41:06 -0300

    v11

--- invitation-app/src/components/GiftInfo/index.tsx
+++ invitation-app/src/components/GiftInfo/index.tsx
@@ -3,17 +3,6 @@ import { motion, useInView } from "framer-motion";
 import ScrollChevrons from "../ScrollChevrons";
 import "./styles.css";
 
-const PrexIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="prex-svg"
-  >
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-  </svg>
-);
-
 const GiftIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
